7ede05d91627030a830f7b90210d7532508b3bd7
Amber Williams <awilliams1234@live.com>
2020-04-06 01:40:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -99,7 +99,7 @@ class TicTacToe
 
   def play
       turn until over?
-       if self.won? || self.draw?
+       if self.won?
       puts "Congratulations #{winner}!"
        else self.draw?
       puts "Cat's Game!"
