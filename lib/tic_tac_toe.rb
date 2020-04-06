37ce30f131d04b6abc02ef32fb2ae799878d0e67
Amber Williams <awilliams1234@live.com>
2020-04-06 01:47:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,7 @@ class TicTacToe
   def won?
     win_combo = nil
     WIN_COMBINATIONS.each do |arr|
-      if @board[arr[0]] == @board[arr[1]] && @board[arr[1]]== @board[arr[2]] #&& position_taken?[arr[0]]
+      if @board[arr[0]] == @board[arr[1]] && @board[arr[1]]== @board[arr[2]] && position_taken?(arr[0])
         win_combo = arr
       end
     end
